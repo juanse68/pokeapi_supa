e3f95e3ea0fb00cea9f69f2bb99e173fd5c41a62
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-05-15 20:33:41 +0000

    creacion del login.js

--- proyecto/src/supabase.js
+++ proyecto/src/supabase.js
@@ -1,6 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'aqui la url de supabase';
-const supabaseKey = 'aqui el codigo de supabase';
-export const supabase = createClient(supabaseUrl,
+
+const supabaseUrl = 'https://cikcadmhhahaevvlbdyb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpa2NhZG1oaGFoYWV2dmxiZHliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMzc3NzksImV4cCI6MjA2MjkxMzc3OX0.ByE4l8Cysk2Rbm_U83ppiLjBrPccFTpmzBU6PicwX6w';
+export const supabase = createClient(supabaseUrl, supabaseKey);
